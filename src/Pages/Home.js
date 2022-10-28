055ac6ef03774ed7e72631a5fa922a21951e559a
bajrang barala <bajarangbarala@gmail.com>
2022-10-28 08:02:27 +0530

    banner

---
+++ src/Pages/Home.js
@@ -0,0 +1,19 @@
+import { Container } from '@mui/material'
+import React from 'react'
+// import Circle from '../components/banner/Circle'
+import Content from '../components/banner/maincontent/Content'
+
+
+function Home() {
+  return (
+    <Container maxWidth="xl">
+        <Content/>
+        {/* <Circle/> */}
+        {/* <About/> */}
+        {/* <Carats/> */}
+        {/* <Courses/> */}
+    </Container>
+  )
+}
+
+export default Home
