0826ec985a6b781563cfbc85e639243c2bc8be53
bajrang barala <bajarangbarala@gmail.com>
2022-10-31 17:00:14 +0530

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+const Footer = () => {
+    return (
+        <Box>
+            <Typography variant="h3">footer my webside</Typography>
+        </Box>
+    )
+}
+
+export default Footer
