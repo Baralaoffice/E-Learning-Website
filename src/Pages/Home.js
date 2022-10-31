52c230a51f68244e7b41f0be83bfd1f8663d3f2e
bajrang barala <bajarangbarala@gmail.com>
2022-10-31 16:30:48 +0530

    cart

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,7 @@ import React from 'react'
 import About from '../components/About/About'
 import Circle from '../components/banner/Circle'
 import Content from '../components/banner/maincontent/Content'
+import Carts from '../components/Cartcomponents/Carts'
 
 
 function Home() {
@@ -11,6 +12,7 @@ function Home() {
       <Content />
       <Circle />
       <About/>
+      <Carts/>
         
     </Container>
   )
