5ca986a288ccfaa6385fb91074f506c5d947b5ff
bajrang barala <bajarangbarala@gmail.com>
2022-10-31 15:57:38 +0530

    about

--- src/Pages/Abouts.js
+++ src/Pages/Abouts.js
@@ -7,7 +7,6 @@ function Abouts() {
     <Box sx={{marginTop:'70px'}}>
         <Container maxWidth="xl">
             <About/>
-            
         </Container>
     </Box>
   )
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,6 @@
 import { Container } from '@mui/material'
 import React from 'react'
+import About from '../components/About/About'
 import Circle from '../components/banner/Circle'
 import Content from '../components/banner/maincontent/Content'
 
@@ -7,11 +8,10 @@ import Content from '../components/banner/maincontent/Content'
 function Home() {
   return (
     <Container maxWidth="xl">
-        <Content/>
-        <Circle/>
-        {/* <About/> */}
-        {/* <Carats/> */}
-        {/* <Courses/> */}
+      <Content />
+      <Circle />
+      <About/>
+        
     </Container>
   )
 }
